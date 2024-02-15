523a10f352a0cc5399181dd24a7c5e4b66cf1fc2
Dmitriy Smolin <45199267+DmitriySmolin@users.noreply.github.com>
2024-02-15 12:52:25 +0400

    update linearSearch.php

--- linearSearch.php
+++ linearSearch.php
@@ -2,17 +2,23 @@
 
 $arr = [1, 4, 5, 8, 5, 1, 2, 7, 5, 2, 1];
 
-function linearSearch($arr, $item) {
+
+function linearSearch($arr, $item, $count = null) {
 	$length = count($arr);
 
 	for ($i = 0; $i < $length; $i += 1) {
+
+		$count += 1;
+
 		if ($arr[$i] === $item) {
-			return $i;
+			return ['key' => $i,
+				'count' => $count];
 		}
 	}
 
 	return null;
 }
 
-print_r(linearSearch($arr, 8));
-print_r(linearSearch($arr, 111));
+$data = linearSearch($arr, 8, 0);
+
+print_r('Индекс элемента: '. $data['key']."\n".'Количество итераций поиска: '. $data['count']);
